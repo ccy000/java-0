f38ad9e462051279b1acc1193496b97841e59248
ccy000 <742655967@qq.com>
2019-03-27 19:25:13 +0800

    A+B

---
+++ src/java测试/B.java
@@ -0,0 +1,12 @@
+package java≤‚ ‘;
+
+import java.util.Scanner;
+
+public class B {
+public static void main(String[] args) {
+    Scanner cin = new Scanner(System.in);
+    int a = cin.nextInt();
+    int b = cin.nextInt();
+    System.out.println(a+b);
+}
+}
